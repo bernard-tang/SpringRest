f557a969bb0ff6c7a76a45033fab81ebb2d3f2f2
bernard <bernarddeng@hotmail.com>
2024-07-05 10:31:51 +0800

    include readme and changes to login controller

--- src/main/java/com/example/accessingdatajpa/controller/UserController.java
+++ src/main/java/com/example/accessingdatajpa/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.accessingdatajpa.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +30,40 @@ public class UserController {
 //    @Autowired
 //    private BCryptPasswordEncoder passwordEncoder;
 
-    @PostMapping("/login")
+    @PostMapping("/login1")
     public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
+        User user = userRepository.findByUsername(username.toUpperCase());
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+        
+        if(user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+        	final String token = jwtUtil.generateToken(user);
+        	return ResponseEntity.ok(token);
+        }
+        else
+        	return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+
+//        if (passwordEncoder.matches(password, user.getPassword())) {
+//            return ResponseEntity.ok("Login successful for " + username);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+//        }
+    }
+    
+    
+    @PostMapping("/login")
+    public ResponseEntity<String> login1(@RequestBody Map<String, String> payload) {
+    	
+    	if(payload.get("username").isEmpty() && payload.get("password").isEmpty())
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+    	
+    	String username = payload.get("username");
+    	String password = payload.get("password");
+    	
+    	log.info("blacklist token " + username);
+    	
         User user = userRepository.findByUsername(username.toUpperCase());
 
         if (user == null) {
